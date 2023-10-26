0244ff8af720d58bd0372bb14a3e82f620045c53
REFICAHYANI <reficahyani92@gmail.com>
2023-10-26 16:05:39 +0700

    first commit

---
+++ composables/constants/category.ts
@@ -0,0 +1,26 @@
+export const category: Category[] = [
+    {
+    id: 1,
+    name: "SAPI JAWA",
+    },
+    {
+    id: 2,
+    name: "SAPI METAL",
+    },
+    {
+    id: 3,
+    name: "SAPI LEMOSIN",
+    },
+    {
+        id: 4,
+        name: "KERBAU LOKAL",
+        },
+        {
+            id: 5,
+            name: "KERBAU ALBINO",
+            },
+    ]
+    export interface Category {
+    id? : number,
+    name?: string,
+    }
